0df0153a710b71400cb3a393dce28dfbb5d7e44d
Monica Mishra <monica.mishra@sendgrid.com>
2013-06-21 09:55:52 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstA::Application.config.secret_token = '9e81703fdafa7ec1782d3af106114f6430cd1d38c21002b88747c662d22acda82395075480c562ed1f510b0db271b948937c2c009405da929c099a0bbce8ec76'
